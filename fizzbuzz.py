a6549dfbafb8c92af74c80aac40e6a4ad6c70c0b
Dmitriy Arkhipchuk <dmitriyarkhipchuk@gmail.com>
2024-08-04 19:52:14 -0700

    upload

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,21 @@
-# add your code here
+#print numbers from 1 to 100
+#if the number is divisible by 3 print Fizz
+#if the number is divisible by 5 print Buzz
+#if the number is divisible by 3 and 5 print FizzBuzz
+
+x = 1
+while x <= 100:
+    if x % 3 == 0 and x % 5 == 0:
+        print ("FizzBuzz")
+    elif x % 3 == 0:
+        print ("Fizz")
+    elif x % 5 == 0:
+        print ("Buzz")
+    else:
+        print (x)
+    x = x + 1
+else:
+    print("\nAll Done")
+    
+
 
